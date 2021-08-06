49b47fde22975d77550d1f3ee3525459c9bfe32e
PURE-cell <1638595707@qq.com>
2021-08-06 08:37:44 +0800

    Mybatis-plus的学习日记代码!

--- src/test/java/com/chen/mybatis_plus/MybatisPlusApplicationTests.java
+++ src/test/java/com/chen/mybatis_plus/MybatisPlusApplicationTests.java
@@ -1,5 +1,6 @@
 package com.chen.mybatis_plus;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,7 +9,10 @@ class MybatisPlusApplicationTests {
 
     @Test
     void contextLoads() {
-
+        String a = " ";
+        System.out.println(a);
+        System.out.println(StringUtils.isNotEmpty(a));
+        System.out.println(StringUtils.isNotBlank(a));
     }
 
 }
